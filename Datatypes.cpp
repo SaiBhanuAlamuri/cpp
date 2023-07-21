323d600399cb5e3347f7f8823ad1bd6afaa58844
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-07-21 10:49:06 +0530

    Add files via upload

---
+++ Datatypes.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	char grade='A';
+	string sentence="This my life";
+	int age=52;
+	float per=2.00;
+	double gpa=2.3;
+	bool ismale="False";
+	cout<< grade<<"Good" <<endl;
+	cout<< sentence <<": A sentence type"<<endl;
+	cout<<"My age is:"<<age<<endl;
+	cout<<"My percentage is:"<<per<<endl;
+	cout<<"My Gender:"<<ismale <<endl;
+}
